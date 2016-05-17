07a83e362c40cfe8c7460d37cc249782b03f1c13
99cms <2811834391@qq.com>
2016-05-17 21:49:14 +0800

    add person class

---
+++ weibo/weibo/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  weibo
+//
+//  Created by 99cms on 16/5/17.
+//  Copyright © 2016年 99cms. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ weibo/weibo/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  weibo
+//
+//  Created by 99cms on 16/5/17.
+//  Copyright © 2016年 99cms. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
